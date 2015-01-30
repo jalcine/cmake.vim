08bf716e7e75f40cd17ef7c41e46944ab04bde14
Jacky Alciné <me@jalcine.me>
2015-01-30 12:18:47 -0500

    ctags: Tweaked logic for making ctags.
      * ctags: Check for b:cmake_target first.
      * test: Fix up ctags + path.

--- spec/path_spec.rb
+++ spec/path_spec.rb
@@ -14,10 +14,11 @@ describe 'cmake.vim#path' do
       before(:each) do
         vim.command 'au! cmake.vim'
         cmake.create_new_project
-        cmake.configure({
-          options: ['-G ' + opts[:generator] ]
-        })
+        cmake.configure(
+          options: ['-G ' + opts[:generator]]
+        )
         vim.command "let g:cmake_build_toolchain='#{ext}'"
+        vim.command 'call cmake#augroup#init()'
         vim.edit 'binary_main.cpp'
       end
 
@@ -35,7 +36,7 @@ describe 'cmake.vim#path' do
         end
 
         it 'sets itself to g:cmake_old_path' do
-          extra_paths = [ '/usr/local/include/', '/usr/include/' ]
+          extra_paths = ['/usr/local/include/', '/usr/include/']
           vim.command "let g:cmake_old_path='#{extra_paths.join(',')}'"
           vim.command 'call cmake#path#reset()'
           expect(vim_paths).to_not be_empty
@@ -74,15 +75,18 @@ describe 'cmake.vim#path' do
 
       describe '#update' do
         it 'sets the path to the buffer-local path variable' do
-          new_paths = [ Dir.mktmpdir, Dir.mktmpdir, Dir.mktmpdir ]
+          new_paths = [Dir.mktmpdir, Dir.mktmpdir, Dir.mktmpdir]
           vim.command "call cmake#path#update(#{new_paths.to_json})"
           new_paths.each { | new_path | expect(vim_paths).to include(new_path) }
         end
 
         it 'sets old paths stored in "g:cmake_old_path"' do
-          cmake_old_paths = validate_json_response 'echo split(g:cmake_old_path, ",")'
+          cmake_old_paths =
+            validate_json_response 'echo split(g:cmake_old_path, ",")'
           cmake_old_paths.uniq!
-          cmake_old_paths.each { | a_path | expect(vim_paths).to include(a_path) unless a_path.empty? }
+          cmake_old_paths.each do | a_path |
+            expect(vim_paths).to include(a_path) unless a_path.empty?
+          end
         end
 
         it 'ensures that the path list is composed only of unique paths' do
