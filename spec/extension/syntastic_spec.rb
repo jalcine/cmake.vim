33b6f1d54ee748ea95081a6b1ddc6f8940e389ef
Jacky Alciné <me@jalcine.me>
2015-02-01 16:20:33 -0500

    [wip] spec: Ensure that Syntastic plugin is loaded.

--- spec/extension/syntastic_spec.rb
+++ spec/extension/syntastic_spec.rb
@@ -1,31 +1,50 @@
 require 'spec_helper'
 
 describe 'cmake#extension#syntastic' do
-  before(:each) do
-    plugin_directory = File.expand_path('../../', __FILE__)
-    vim.add_plugin(plugin_directory, 'spec/plugins/vim/syntastic')
-    cmake.create_new
-    cmake.configure
-    vim.command 'call cmake#targets#cache()'
-  end
+  {
+    gnumake: {
+      generator: 'Unix\ Makefiles'
+    },
+    ninja: {
+      generator: 'Ninja'
+    }
+  }.each do | ext, opts |
+    context "when using a #{ext} build system" do
+      before(:each) do
+        vim.command 'let g:cmake_build_toolchain="' + ext.to_s + '"'
+        vim.command 'au! cmake.vim'
+        cmake.create_new_project(
+          options: ['-G ' + opts[:generator]]
+        )
+        cmake.configure(
+          options: ['-G ' + opts[:generator]]
+        )
 
-  describe '#inject' do
-    context 'function existence' do
-      it 'does exist when not called' do
-        expect(function_exists? 'cmake#extension#syntastic#inject(args)').to eql(true)
+        plugin_directory = File.expand_path('../../../', __FILE__) +
+          '/spec/plugins/vim/syntastic'
+        vim.append_runtimepath(plugin_directory)
       end
 
-      it 'does exist when called' do
-        vim.command 'call cmake#extension#syntastic#inject({"target":"sample-binary"})'
-        expect(function_exists? 'cmake#extension#syntastic#inject(args)').to eql(true)
-      end
-    end
+      describe '#inject' do
+        context 'function existence' do
+          it 'does exist when not called' do
+            expect(function_exists? 'cmake#extension#syntastic#inject(args)').to eql(true)
+          end
 
-    it 'populates the buffers options' do
-      vim.command 'call cmake#extension#syntastic#inject({"target":"sample-library"})'
-      obtained_includes = validate_response 'echo b:syntastic_cpp_includes'
-      expect(obtained_includes).to_not be_empty
-      expect(obtained_includes).to_not include('[')
+          it 'does exist when called' do
+            vim.command 'call cmake#extension#syntastic#inject({"target":"sample-binary"})'
+            expect(function_exists? 'cmake#extension#syntastic#inject(args)').to eql(true)
+          end
+        end
+
+        it 'populates the buffers options' do
+          vim.command 'call cmake#extension#syntastic#inject({"target":"sample-library"})'
+          obtained_includes = vim.echo 'b:syntastic_cpp_includes'
+          expect(obtained_includes).to_not be_empty
+          expect(obtained_includes).to_not include('[')
+          expect(Dir.exist? obtained_includes).to eql(true)
+        end
+      end
     end
   end
 end
